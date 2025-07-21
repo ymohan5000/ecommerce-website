0597ec84156ec9000fd525723275ba8cdd3e0f4f
Mohan Yadav <ymohan5000@gmail.com>
2025-07-21 20:55:50 +0545

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
   images: {
     unoptimized: true,
   },
-}
+  basePath: '/ecommerce-website', // Required for GitHub Pages!
+  assetPrefix: '/ecommerce-website',
+  experimental: {
+    allowedDevOrigins: ['http://192.168.0.123:3000'],
+  },
+};
 
-export default nextConfig
+export default nextConfig;
